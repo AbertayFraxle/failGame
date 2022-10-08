13ecfecbe6d408c5862da056e38cad2f2f63eb27
AbertayFraxle <91606018+AbertayFraxle@users.noreply.github.com>
2022-10-08 20:33:01 +0100

    lots of general changes

--- game/Assets/Scripts/playerDie.cs
+++ game/Assets/Scripts/playerDie.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.SearchService;
+//using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -27,6 +27,7 @@ public class playerDie : MonoBehaviour
                 controller.enabled = true;
 
                 this.GetComponent<bootPower>().removeBoots();
+                this.GetComponent<coinManage>().resetCoin();
                 pickupMan.GetComponent<reloadObjects>().trigger();
             }
             else
--- game/Assets/Scripts/playerMove.cs
+++ game/Assets/Scripts/playerMove.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Animations;
+//using UnityEditor.Animations;
 using UnityEngine;
 
 public class playerMove : MonoBehaviour
