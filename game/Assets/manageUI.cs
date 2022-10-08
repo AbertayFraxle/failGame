8650364297857b56a604fcbc22c8506f5cbe68a6
AbertayFraxle <91606018+AbertayFraxle@users.noreply.github.com>
2022-10-08 19:39:59 +0100

    hud added (good)

---
+++ game/Assets/manageUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class manageUI : MonoBehaviour
+{
+    [SerializeField]
+    public TextMeshProUGUI lives;
+
+    [SerializeField]
+    public TextMeshProUGUI coins;
+
+    [SerializeField]
+    public Transform player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        lives.text = "x" + player.GetComponent<playerDie>().lives;
+
+        coins.text = player.GetComponent<coinManage>().checkCoins().ToString();
+    }
+}
