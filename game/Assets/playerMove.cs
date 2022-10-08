196fa6e4fa6c93f5cd75bdbc8b0098e27ee54aa4
C-Shep <2101007@uad.ac.uk>
2022-10-08 01:02:57 +0100

    Added 3rd Person Cam
    Also added Player Prefab
    Cinemachine required

--- game/Assets/playerMove.cs
+++ game/Assets/playerMove.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
+    public Transform cam;
     public CharacterController controller;
+
     public float spd = 12f;
     public float grv = -9.8f;
     public float jumpHeight = 3f;
+
+    public float turnSmooth = 0.1f;
+    float turnSmoothVelocity;
 
     public Transform groundCheck;
     public float groundDis = 0.4f;  //ground distance you twat
@@ -17,17 +22,30 @@ public class playerMove : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+        // Update is called once per frame
+        void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDis, groundMask);
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 dir = new Vector3(x,0f,z).normalized;
 
-        controller.Move(move*spd * Time.deltaTime);
+        if(dir.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle, ref turnSmoothVelocity, turnSmooth);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDir.normalized * spd * Time.deltaTime);
+        }
 
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -39,7 +57,7 @@ public class playerMove : MonoBehaviour
             velocity.y = -2f;
         }
 
-            velocity.y += grv * Time.deltaTime;
+        velocity.y += grv * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
 }
